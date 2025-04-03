d777fad3066b71803a0b068934ba1e4bbea470e5
AntonyHROliveira <henritony73@gmail.com>
2025-04-03 18:48:41 -0300

    chore: update

--- Projet01/script/script.js
+++ Projet01/script/script.js
@@ -2,3 +2,8 @@
 // document.getElementById("email").style.display = "none";
 // document.getElementById("password").style.display = "none";
 
+function aparecer(){
+    document.getElementById('demo').style.display="block";
+}
+
+
